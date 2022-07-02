7ddc78151c96050f447adfab8b212bf12ac926db
aki0921 <66670058+aki0921@users.noreply.github.com>
2022-07-02 18:43:20 +0900

    Add files via upload

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -6,3 +6,17 @@ document.getElementById('toggle').addEventListener('click', function() {
   document.getElementById('overrayBottom').classList.toggle('showBottom');
   document.getElementById('nav').classList.toggle('showNav');
 });
+
+const linkText = document.getElementsByTagName('nav')[0].getElementsByTagName('a');
+
+for ( var n of linkText ) {
+  n.addEventListener('click', function() {
+    document.getElementById('line1').classList.remove('line1');
+    document.getElementById('line2').classList.remove('line2');
+    document.getElementById('line3').classList.remove('line3');
+    document.getElementById('overrayTop').classList.remove('showTop');
+    document.getElementById('overrayBottom').classList.remove('showBottom');
+    document.getElementById('nav').classList.remove('showNav');
+  });
+};
+
